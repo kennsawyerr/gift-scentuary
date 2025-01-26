043c977dc874ef2be039a8ede29258b50140c578
Kehinde Sawyerr <kensawyerr@gmail.com>
2025-01-26 22:31:22 +0100

    starting backend today

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+// Admin log in
+// Admin can edit perfume names and prices
+// bank transfers with a code to use to track record of sales. ready to checkout, use code "machalla as desc"
+
+// USERS
+// okay login,
+// have history list
+// profile picture and name showing
+// have made for you,"because you liked ${USERS_LAST_PURCHASE} try ${NEWER_UPLOADS}. lock it up at ${NEWER_UPLOADS_PRICE} "
+
+
+// STACK 
+// mongodb for database
+// 
+
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
